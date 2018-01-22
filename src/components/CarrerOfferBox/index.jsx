3c42b38f8acfaa0f55b52b93c4c60ae92442728f
BenTheTank <benjamin.tenke@cofinpro.de>
2018-01-22 22:43:16 +0100

    changed carousel width

--- src/components/CarrerOfferBox/index.jsx
+++ src/components/CarrerOfferBox/index.jsx
@@ -52,58 +52,76 @@ class CarrerOfferBox extends React.Component {
       }
     }
 
-    return (
-      <div>
-        {buckets.length > 0
-          ? buckets.map((anzeige, i) => {
-              if (i < 3) {
-                return (
-                  <Link
-                    to={pathPrefix + '/stellenanzeige/' + anzeige.node.url}
-                    className="text-dark"
-                    key={'anzeige-link-' + i}
-                  >
-                    <div
-                      className="row padding-sm-top"
-                      key={'anzeige-row-' + i}
+    if (buckets.length > 0) {
+      return (
+        <div>
+          {buckets.length > 0
+            ? buckets.map((anzeige, i) => {
+                if (i < 3) {
+                  return (
+                    <Link
+                      to={pathPrefix + '/stellenanzeige/' + anzeige.node.url}
+                      className="text-dark"
+                      key={'anzeige-link-' + i}
                     >
                       <div
-                        className="col-10 col-md-10 col-lg-10"
-                        key={'anzeige-col-' + i}
-                      >
-                        <div className="" key={'anzeige-padding-' + i}>
-                          <p
-                            className="no-margin-bottom text-left"
-                            key={'anzeige-text-' + i}
-                          >
-                            {anzeige.node.titel}
-                          </p>
-                        </div>
-                      </div>
-                      <div
-                        className="col-2 col-md-2 col-lg-2 align-self-center"
-                        key={'anzeige-col-arrow-' + i}
+                        className="row padding-sm-top"
+                        key={'anzeige-row-' + i}
                       >
                         <div
-                          className="text-primary"
-                          key={'anzeige-arrow-box-' + i}
+                          className="col-10 col-md-10 col-lg-10"
+                          key={'anzeige-col-' + i}
                         >
-                          <span
-                            className="h4 text-primary font-weight-bold stellenanzeige-link"
-                            key={'anzeige-arrow-box-icon-' + i}
-                          >
+                          <div className="" key={'anzeige-padding-' + i}>
+                            <p
+                              className="no-margin-bottom text-left"
+                              key={'anzeige-text-' + i}
                             >
-                          </span>
+                              {anzeige.node.titel}
+                            </p>
+                          </div>
+                        </div>
+                        <div
+                          className="col-2 col-md-2 col-lg-2 align-self-center"
+                          key={'anzeige-col-arrow-' + i}
+                        >
+                          <div
+                            className="text-primary"
+                            key={'anzeige-arrow-box-' + i}
+                          >
+                            <span
+                              className="h4 text-primary font-weight-bold stellenanzeige-link"
+                              key={'anzeige-arrow-box-icon-' + i}
+                            >
+                              >
+                            </span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                )
-              }
-            })
-          : null}
-      </div>
-    )
+                    </Link>
+                  )
+                }
+              })
+            : null}
+        </div>
+      )
+    } else {
+      return (
+        <a
+          className="text-dark d-block padding-sm-bottom"
+          target="_blank"
+          href="https://recruitingapp-2586.umantis.com/Vacancies/297/Application/CheckLogin/1?lang=ger"
+        >
+          <p className="no-margin-bottom text-left padding-sm-bottom">
+            Zur Zeit haben wir leider keine freien Stellen, aber wir freuen uns
+            über deine Initiativbewerbung!
+          </p>
+          <p className="text-primary no-margin-bottom d-flex justify-content-between">
+            Jetzt initiativ bewerben <span>></span>
+          </p>
+        </a>
+      )
+    }
   }
 }
 
--- src/components/CarrerOfferPreview/index.jsx
+++ src/components/CarrerOfferPreview/index.jsx
@@ -5,42 +5,51 @@ import get from 'lodash/get'
 import './style.scss'
 
 class CarrerOfferPreview extends React.Component {
-    render() {
-        const { title, employmentType, expiration, locationEmployee, anzeigeId, location, styleClass } = this.props
+  render() {
+    const {
+      title,
+      employmentType,
+      expiration,
+      locationEmployee,
+      anzeigeId,
+      location,
+      styleClass,
+    } = this.props
 
-        const pathPrefix = process.env.NODE_ENV === 'development'
-            ? ''
-            : __PATH_PREFIX__
+    const pathPrefix =
+      process.env.NODE_ENV === 'development' ? '' : __PATH_PREFIX__
 
-        const additionalStyleClass = styleClass !== null ? styleClass : "";
+    const additionalStyleClass = styleClass !== null ? styleClass : ''
 
-        return (
-            <div className={"carrerOfferPreview " + additionalStyleClass}>
-                <div className="padding-sm">
-                <Link to={pathPrefix + "/stellenanzeige/" + anzeigeId} className="text-dark">
-                    <div className="col-12">
-                        <p className="text-left font-weight-bold">
-                            {title.length > 38 ? title.substring(0, 38) + "..." : title}
-                        </p>
-                        <p className="no-margin-bottom">
-                            Art: {employmentType}
-                            <br />
-                            Befristung: {expiration}
-                            <br />
-                            Ort: {locationEmployee}
-                        </p>
-                    </div>
-                    <div className="col-12 text-right">
-                        <span className="h4 text-primary font-weight-bold stellenanzeige-link">
-                            >
-                        </span>
-                    </div>
-    
-                    </Link>
-                </div>
+    return (
+      <div className={'carrerOfferPreview ' + additionalStyleClass}>
+        <div className="padding-sm">
+          <Link
+            to={pathPrefix + '/stellenanzeige/' + anzeigeId}
+            className="text-dark"
+          >
+            <div className="col-12">
+              <p className="text-left font-weight-bold">
+                {title.length > 48 ? title.substring(0, 48) + '...' : title}
+              </p>
+              <p className="no-margin-bottom">
+                Art: {employmentType}
+                <br />
+                Befristung: {expiration}
+                <br />
+                Ort: {locationEmployee}
+              </p>
             </div>
-        )
-    }
+            <div className="col-12 text-right">
+              <span className="h4 text-primary font-weight-bold stellenanzeige-link">
+                >
+              </span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CarrerOfferPreview
