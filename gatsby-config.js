ffa54aa5cac96a498e22cfb283bc37c7e9b068bf
BenTheTank <benjamin.tenke@cofinpro.de>
2018-02-19 14:29:52 +0100

    changed again

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,15 +32,11 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
-          },
-          {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
               wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           'gatsby-remark-autolink-headers',
@@ -58,6 +54,5 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-twitter',
   ],
 }
