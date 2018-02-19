fb3f44316946c2d38b69672d25c2f38171a1c2e6
BenTheTank <benjamin.tenke@cofinpro.de>
2018-02-19 14:52:39 +0100

    testing dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,11 +32,15 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-images',
+          },
+          {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
               wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
+          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           'gatsby-remark-autolink-headers',
