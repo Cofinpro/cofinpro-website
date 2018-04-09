315f55e5f1a92ec5a18966e1cd7b1c13efb31eea
BenTheTank <benjamin.tenke@cofinpro.de>
2018-04-09 15:21:14 +0200

    added new images and fixed nullpointer

--- src/components/ContentfulImage/index.jsx
+++ src/components/ContentfulImage/index.jsx
@@ -5,27 +5,31 @@ import get from 'lodash/get'
 import './style.scss'
 
 class ContentfulImage extends React.Component {
-    render() {
-        const { imageFile, styleClasses } = this.props
-        const pathPrefix = process.env.NODE_ENV === 'development'
-            ? ''
-            : __PATH_PREFIX__
+  render() {
+    const { imageFile, styleClasses } = this.props
+    const pathPrefix =
+      process.env.NODE_ENV === 'development' ? '' : __PATH_PREFIX__
 
-        var filePath;
+    var filePath
 
-        if (imageFile == null) {
-            return  null;
-        } else {
-            var fileEnding = imageFile.file.fileName.substring(imageFile.file.fileName.lastIndexOf('.'), imageFile.file.fileName.length)
-            filePath = pathPrefix + '/img/contentful/' + imageFile.id + fileEnding
+    if (imageFile == null || imageFile.file == null) {
+      return null
+    } else {
+      var fileEnding = imageFile.file.fileName.substring(
+        imageFile.file.fileName.lastIndexOf('.'),
+        imageFile.file.fileName.length
+      )
+      filePath = pathPrefix + '/img/contentful/' + imageFile.id + fileEnding
 
-            return (
-                <img
-                    src={filePath}
-                    className={styleClasses == null ? "img-fluid" : styleClasses} alt={imageFile == null ? "" : imageFile.description} />
-            )
-        }
+      return (
+        <img
+          src={filePath}
+          className={styleClasses == null ? 'img-fluid' : styleClasses}
+          alt={imageFile == null ? '' : imageFile.description}
+        />
+      )
     }
+  }
 }
 
 export default ContentfulImage
