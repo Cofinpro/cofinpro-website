dc0d6d4ffb4bb24fb1fb14f529531bf151e26d40
Kevin Kessenich <kevin.kessenich@cofinpro.de>
2018-07-19 16:24:49 +0200

    move beratungsfelder

---
+++ src/pages/beratungsfelder/fach/index.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+import LinkButton from '../../../components/buttons/LinkButton'
+
+import {
+  ImageWrapper,
+  SOURCE_TYP_PLACEHOLDER,
+} from '../../../components/images/ImageWrapper'
+
+class BeratungsfelderFachTemplate extends React.Component {
+  render() {
+    const pathPrefix =
+      process.env.NODE_ENV === 'development' ? '' : __PATH_PREFIX__
+
+    return (
+      <div>
+        <div className="container-fluid no-gutters">
+          <div className="row">
+            <div className="col-md-12">
+              <ImageWrapper
+                sourceType={SOURCE_TYP_PLACEHOLDER}
+                source={{
+                  width: 2000,
+                  height: 800,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container margin-40-top">
+          <div className="row">
+            <div className="col-md-2">
+              <ImageWrapper
+                sourceType={SOURCE_TYP_PLACEHOLDER}
+                source={{
+                  width: 128,
+                  height: 132,
+                }}
+              />
+            </div>
+          </div>
+          <div className="row margin-40-top">
+            <div className="col-md-7">
+              <h1 className="h1">Fachberatung</h1>
+              <h2 className="d-none d-md-block">
+                Die Experten <br />für Kredit und Wertpapier
+              </h2>
+              <p className="d-block d-md-none">
+                Die Experten für Kredit und Wertpapier
+              </p>
+              <p className="text-left margin-40-top margin-20-bottom margin-xs-20-top">
+                {' '}
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className="col-md-5">
+              <LinkButton
+                text="Kreditgeschäft"
+                styleSpan="btn-lg btn-block margin-20-bottom margin-20-top"
+                path="/beratungsfelder/kredit"
+              />
+              <LinkButton
+                text="Wertpapiergeschäft"
+                styleSpan="btn-lg btn-block"
+                path="/beratungsfelder/wertpapier"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3" />
+            <div className="col-md-6">
+              <p className="text-left margin-40-top margin-xs-40-top">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className="col-md-3" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BeratungsfelderFachTemplate
---
+++ src/pages/beratungsfelder/wertpapier/index.jsx
@@ -0,0 +1,181 @@
+import React from 'react'
+
+import RelevanteFokusthemen from '../../../components/RelevanteFokusthemen'
+import ReferenzAndDownload from '../../../components/ReferenzAndDownload'
+import FooterNavigation from '../../../components/FooterNavigation'
+
+import {
+  ImageWrapper,
+  SOURCE_TYP_PLACEHOLDER,
+} from '../../../components/images/ImageWrapper'
+
+class BeratungsfelderWertpapierTemplate extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container-fluid no-gutters">
+          <div className="row">
+            <div className="col-md-md-12">
+              <ImageWrapper
+                sourceType={SOURCE_TYP_PLACEHOLDER}
+                source={{
+                  width: 2000,
+                  height: 800,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <img
+                src="http://via.placeholder.com/128x132"
+                alt="placeholder"
+                className="img-fluid margin-20-bottom margin-20-top"
+              />
+              <h1>Wertpapiergeschäft</h1>
+              <h2 className="margin-20-top">
+                Fachberatung im<br />Wertpapierumfeld
+              </h2>
+              <h4 className="margin-40-top">
+                Unsere erfahrenen Berater verbinden ihre <br />
+                langjährige Erfahrung im Wertpapiergeschäft <br />
+                mit methodischem Wissen und einer hohen <br />
+                projektmanagmentkompetenz.
+              </h4>
+            </div>
+
+            <div className="col-md-4">
+              <RelevanteFokusthemen />
+            </div>
+          </div>
+          <div className="row margin-120-top">
+            <div className="col-md-6">
+              <h2>
+                LOREM IPSUM dolor <br />
+                sit amet lorem ipsum
+              </h2>
+              <p className="text-left margin-20-top">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+              </p>
+            </div>
+          </div>
+          <div className="row margin-120-top">
+            <div className="col-md-12">
+              <h2 className="text-primary text-center">Wertpapierprozess</h2>
+              <img
+                src="http://via.placeholder.com/937x90"
+                alt="placeholder"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+          <div className="row margin-80-top">
+            <div className="col-md-12">
+              <h2 className="text-primary text-center">Investmentprozess</h2>
+              <img
+                src="http://via.placeholder.com/937x90"
+                alt="placeholder"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+          <div className="row margin-100-top">
+            <div className="col-md-6" />
+            <div className="col-md-6">
+              <h2>
+                LOREM IPSUM dolor <br />
+                sit amet lorem ipsum
+              </h2>
+              <p className="text-left margin-20-top">
+                {' '}
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet.
+              </p>
+              <p className="text-left margin-20-top">
+                {' '}
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+          </div>
+          <div className="row margin-60-top">
+            <div className="col-md-6">
+              <h3>
+                Mit besonderer Expertise und<br />
+                Praxiserfahrung berät Cofinpro<br />
+                Sie in den Themen:
+              </h3>
+              <img
+                src="http://via.placeholder.com/448x267"
+                alt="placeholder"
+                className="img-fluid margin-100-top"
+              />
+              <img
+                src="http://via.placeholder.com/288x172"
+                alt="placeholder"
+                className="img-fluid margin-40-top"
+              />
+              <div className="row">
+                <div className="col-md-2" />
+                <div className="col-md-10">
+                  <img
+                    src="http://via.placeholder.com/288x172"
+                    alt="placeholder"
+                    className="img-fluid margin-40-top"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 margin-140-top">
+              <img
+                src="http://via.placeholder.com/288x172"
+                alt="placeholder"
+                className="mx-auto d-block margin-40-top"
+              />
+              <img
+                src="http://via.placeholder.com/448x267"
+                alt="placeholder"
+                className="mx-auto d-block margin-40-top"
+              />
+
+              <img
+                src="http://via.placeholder.com/448x267"
+                alt="placeholder"
+                className="mx-auto d-block margin-40-top"
+              />
+            </div>
+          </div>
+          <div className="margin-100-top">
+            <ReferenzAndDownload />
+          </div>
+        </div>
+        <div className="margin-140-top">
+          <FooterNavigation backButtonPath="/beratungsfelder" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BeratungsfelderWertpapierTemplate
